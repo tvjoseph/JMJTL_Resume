6cf95c456cc1cf75ee63ce6ad935185f77f0236e
thomas.joseph <joevad@gmail.com>
2020-04-13 15:14:58 +0530

    Commit as on 13th April

--- Resume/Datasets/data_Loader.py
+++ Resume/Datasets/data_Loader.py
@@ -10,7 +10,11 @@ from PIL import Image
 import pytesseract
 import sys
 from pdf2image import convert_from_path
-
+import docx
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
 
 
 class DataProcessor:
@@ -19,7 +23,13 @@ class DataProcessor:
     def __init__(self,configfile):
         # This is the first method in the DataProcessor class
         self.config = configfile
-
+        self.nlp = spacy.load('en_core_web_sm')
+        #self.tokenizer = bertTokenizer
+        #self.model = bertModel
+    '''    
+    The below method looks at the path where all the files reside and then return the path of all the files in a list 
+    for the next process
+    '''
     def preprocessor(self):
         # This is the method to first load all the files from the path
 
@@ -27,7 +37,9 @@ class DataProcessor:
         # Starting two empty lists to load the pdf documents and word document lists
         print('This is the resume file path',Resume_File_Path)
         pdf_list = []
+        pdf_names = []
         doc_list = []
+        doc_names = []
         for (path,_,files) in os.walk(Resume_File_Path):
             # In the above walk you get the full path of the resume like /media/acer/7DC832E057A5BDB1/JMJTL/Tomslabs/Datasets/ResumeData and
             # The files in the path in the form of a list.
@@ -36,17 +48,59 @@ class DataProcessor:
                 ext = os.path.splitext(file)[-1]
                 if ext == '.docx':
                     doc_list.append(path+'/'+file)
+                    doc_names.append(os.path.splitext(file)[-2])
                 elif ext == '.pdf':
                     pdf_list.append(path+'/'+file)
+                    pdf_names.append(os.path.splitext(file)[-2])
 
-        return pdf_list,doc_list
+        return pdf_list,pdf_names,doc_list,doc_names
+
+    '''    
+        The below method is a utility function which is used in the subsequent program. This method splits any strings as per the new lines
+        and then joins them together
+    '''
+
+    def textProcessor(self,text):
+        # Splitting the text based on sentences
+        text = text.split('\n\n')
+        textList = []
+        for strings in text:
+            tempText = strings.split('\n')
+            tempText = ' '.join(tempText)
+            textList.append(tempText)
+        return textList
+
+    '''    
+        The below method implements the Named entity recognition method for text in the resume data list
+    '''
+
+    # The below is the function to create the NER from the text
+    def nerMaker(self,indResume):
+        # indResume is the list of resume text generated from pdf documents and docx documents
+        # Define a dictionary to store the values
+        resNer = {}
+        # Loop through each of the text in the indResume list
+        for resline in indResume:
+            # Parse through the self.nlp spacy object defined in the init program
+            doc = self.nlp(resline)
+            # Extract the labels and its entity
+            for X in doc.ents:
+                if X.label_ in resNer:
+                    resNer[X.label_].append(X.text)
+                else:
+                    resNer[X.label_] = [X.text]
+        return resNer
+
+    '''    
+     The below method is an important method for processing pdf documents.
+    '''
 
     def pdfProcessor(self):
         # Get the pdf files from the method preprocessor
-        pdf_list,_ = self.preprocessor()
+        pdf_list,pdf_names,_,_ = self.preprocessor()
         # Loop over all the pdf files
         pdfResume = []
-        for pdfFile in pdf_list:
+        for i,pdfFile in enumerate(pdf_list):
             # Convert the pdf file into multiple images
             pages = convert_from_path(pdfFile, 500)
             # Starting an empty list to store the text details
@@ -54,12 +108,59 @@ class DataProcessor:
             for page in pages:
                  # Recognize the text as string in image using pytesserct
                  text = str(((pytesseract.image_to_string(page))))
-                 # Splitting the text based on sentences
-                 text = text.split('.')
-                 resDetails.append(text)
+                 tempText = self.textProcessor(text)
+                 for textline in tempText:
+                     resDetails.append(textline)
+            # Get the named entities from resDetails
+            resNer = self.nerMaker(resDetails)
+            resNer['ResumeName'] = pdf_names[i]
+            resDetails.append(resNer)
             # Appending the overall details of all consumers into the bigger list
             pdfResume.append(resDetails)
         return pdfResume
+
+    def docxProcessor(self):
+        # Get the pdf files from the method preprocessor
+        _,_,doc_list,doc_names = self.preprocessor()
+        # Loop over all the doc files
+        docResume = []
+        for i,docFile in enumerate(doc_list):
+            # Read the document in multiple docs
+            doc = docx.Document(docFile)
+            # Starting an empty list to store the text details
+            indResume = []
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)
+                '\n'.join(fullText)
+            # Removing empty spaces
+            list2 = [e for e in fullText if e]
+            # Further processing for tab spaced docs
+            for list in list2:
+                list = list.split('\t')
+                for sublist in list:
+                    list3 = "".join(sublist)
+                    indResume.append(list3)
+            # Removing blank spaces in the resume list
+            indResume = [e for e in indResume if e]
+            # Get the named entities from resDetails
+            resNer = self.nerMaker(indResume)
+            resNer['ResumeName'] = doc_names[i]
+            indResume.append(resNer)
+            # Appending the individual resumes
+            docResume.append(indResume)
+
+        return docResume
+
+    # This method to be completed
+
+    def allDocs(self):
+        # This is a method to consolidate all the records
+        pdfDocs = self.pdfProcessor()
+        docDocs = self.docxProcessor()
+
+        return pdfDocs,docDocs
+
 
 
 
--- Resume/resumeDriver.py
+++ Resume/resumeDriver.py
@@ -8,8 +8,16 @@ Lord bless this attempt of yours
 import argparse
 import sys
 from configparser import ConfigParser
-
+from Modelfiles import Model
 from Datasets import DataProcessor
+
+import pickle
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from pymongo import MongoClient
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
 
 
 
@@ -37,10 +45,108 @@ The data loading module will have the following structure
 4. Process the files and then store in a single data set with each line classified as to what category of feature it is
 
 '''
+# First import the bert model and evaluate it
+
+print('[INFO] loading and initializing BERT models......')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# Load pre-trained model (weights)
+model = BertModel.from_pretrained('bert-base-uncased')
+# Put the model in "evaluation" mode, meaning feed-forward operation.
+model.eval()
+
+print('[INFO] loading the estimator models......')
+
+# Importing the estimator
+estimator_path = default_cfg.get('Modelfiles','estimator')
+estimator_doc = open(estimator_path,"rb")
+# Loading the estimator
+estimator = pickle.load(estimator_doc)
+
+print('[INFO] starting the data processing process .............')
+
 dl = DataProcessor(default_cfg)
 
-pdf_list = dl.pdfProcessor()
-print('PDF list',len(pdf_list))
-print(pdf_list[0])
+pdfDocs,docDocs = dl.allDocs()
+
+# Instantiating the models
+
+modelBuilding = Model(model,tokenizer)
+
+#tempVec = modelBuilding.genMeanvec(str(docDocs[0][0]))
+
+print('[INFO] Starting the consolidation of the resume documents')
+
+# Consolidating all documents into one list
+docTypes = [pdfDocs,docDocs]
+
+# Opening the mongoDB client
+
+client = MongoClient(port=27017)
+db=client.Subjects
+
+# Starting a for loop to go through each type of document
+
+for doctype in docTypes:
+    # Taking one of the resume format and generating two empty lists
+    # The first list is to store the raw features
+    # The second list is to store the vectors of the features
+    allResume = []
+    allResume_vector = []
+    for resume in doctype:
+        # Creating a dictionary for storing each customers details
+        indResDic = {'ResumeName': [], 'Name': [], 'Academic_credentials': [], 'Certifications': [], 'Date': [],
+                     'Key_projects': [], 'Profile_summary': [], 'Skills': [], 'Work_exp': [], 'accolades': [],
+                     'email': [], 'location': [], 'phone': []}
+        indResDic_docs = {'ResumeName': [], 'Name': [], 'Academic_credentials': [], 'Certifications': [],
+                          'Date': [], 'Key_projects': [], 'Profile_summary': [], 'Skills': [], 'Work_exp': [],
+                          'accolades': [], 'email': [], 'location': [], 'phone': []}
+        # First get the resume Name
+        indResDic['ResumeName'].append(resume[-1]['ResumeName'])
+        indResDic_docs['ResumeName'].append(resume[-1]['ResumeName'])
+        # Loop through all the text in the resume
+        for i in range(0, len(resume[0:-1])):
+            docs = resume[0:-1][i].split(';')
+            for doc in docs:
+                tempVector = modelBuilding.genMeanvec(str(doc))
+                tempVector = tempVector.reshape((1, tempVector.shape[0]))
+                pred = estimator.predict(tempVector)
+                if pred[0] == 'BS':
+                    continue
+                else:
+                    indResDic[pred[0]].append(tempVector)
+                    indResDic_docs[pred[0]].append(doc)
+        # Updating the last dictionary of NER components in the just created dictionary
+        indResDic_docs.update(resume[-1])
+        # inserting the record into the MOngo DB collections
+        db.processedResume.insert_one(indResDic_docs)
+        #db.processedResumeVec.insert_one(indResDic)
+        # Inserting the details into the lists
+    allResume.append(indResDic_docs)
+    allResume_vector.append(indResDic)
+
+print('[INFO] completed the processes. Printing results')
+
+print('Resume lenght',len(allResume))
+print(allResume[0])
+#print('Doc list',len(docDocs))
+#print(docDocs[0])
+
+# Pickling The resume docs
+
+##################################################################################
+
+output_File_Path = default_cfg.get('Outputfiles','scrapDocs')
+
+vecFilepath = output_File_Path + '/' + "vector.pickle"
+docFilepath = output_File_Path + '/' + "rawDoc.pickle"
+
+pickle_vector = open(vecFilepath,"wb")
+pickle.dump(allResume_vector, pickle_vector)
+pickle_vector.close()
+
+pickle_doc = open(docFilepath,"wb")
+pickle.dump(allResume, pickle_doc)
+pickle_doc.close()
 
 #input_data = preprocess(default_cfg)
+
